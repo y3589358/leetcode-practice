483520e0e128c2bbcd314e0484530c0541aab799
y3589358 <tyang28@hawk.iit.edu>
2022-01-29 19:39:42 -0600

    first commit

---
+++ 78-Subsets.js
@@ -0,0 +1,36 @@
+/**
+ *  Requirment:
+    Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+    The solution set must not contain duplicate subsets. Return the solution in any order.
+
+    Example 1:
+
+    Input: nums = [1,2,2]
+    Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+ */
+
+/**
+    Note:
+    before recursive should be sort arr, if exsist 2' and 2'' only chose one when index > i
+ */
+var subsetsWithDup = function(nums) {
+    const res = [];
+    nums.sort();
+    if(nums.length == 0){
+        res.push([]);
+        return;
+    }
+    const dfs = (i,cur)=>{
+        res.push([...cur]);
+        for(let index = i;index < nums.length;index++){
+            if(index != 0 && nums[index] == nums[index-1] && index > i){
+               continue;
+            }
+            cur.push(nums[index]);
+            dfs(index+1,cur);
+            cur.pop();
+        }
+    }
+    dfs(0,[]);
+    return res;
+};
